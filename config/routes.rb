a1de050f0d1b8fba04ff519911db8144f6acd438
Nima Boscarino <nima.boscarino@gmail.com>
2018-11-08 09:54:19 -0800

    i am proud of you

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :sales, only: [:index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
